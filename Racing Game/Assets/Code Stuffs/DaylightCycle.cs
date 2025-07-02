20dc696a2c20dc526f10e9ea19a0941fdfeb5f46
BaileyRMH <BMadiganHennessy2024@elam.co.uk>
2025-07-02 16:11:16 +0100

    unity just crashed lol

--- Racing Game/Assets/Code Stuffs/DaylightCycle.cs
+++ Racing Game/Assets/Code Stuffs/DaylightCycle.cs
@@ -6,17 +6,33 @@ using UnityEngine;
 public class DaylightCycle : MonoBehaviour
 {
 
+    public GameObject Light;
 
+    [Header("Speed Of Rotation")]
+    public float rotationSpeed;
+
+    [Header("How Long Is One Day/Night Cycle?")]
+    public float howManyHours;
+    public float howManyMinutes;
+    public float howManySeconds;
+
+    [Header("Total Time")]
+    public float cycleTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cycleTime = (howManyHours * 60 * 60) + (howManyMinutes * 60) + howManySeconds;
+        rotationSpeed = 360 / cycleTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Light.transform.Rotate(Vector3.right, rotationSpeed);
     }
+
+
+
+
 }
